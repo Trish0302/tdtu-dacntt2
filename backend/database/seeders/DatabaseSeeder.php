005a16279e065460d3786bbf89e0837dea183703
Phan Minh Hiếu <51900741@student.tdtu.edu.vn>
2023-06-15 11:34:20 +0700

    add-seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -24,7 +24,8 @@ class DatabaseSeeder extends Seeder
             FoodSeeder::class,
             StatusSeeder::class,
             VoucherSeeder::class,
-            TransactionSeeder::class
+            TransactionSeeder::class,
+            DeliverySeeder::class
         ]);
     }
 }
