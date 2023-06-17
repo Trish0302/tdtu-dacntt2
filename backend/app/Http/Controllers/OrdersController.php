25beff228c2a8977fb72eda109df59f224702f48
trilnm <letri03022001@gmail.com>
2023-06-17 18:19:01 +0700

    momo payment process and order history (000, 030, 020)

--- backend/app/Http/Controllers/OrdersController.php
+++ backend/app/Http/Controllers/OrdersController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\API\payment\MOMOController;
 use App\Http\Requests\OrdersRequest;
+use App\Models\History;
 use App\Models\Order;
 use App\Models\OrderDetail;
 use Illuminate\Http\Request;
+use stdClass;
 
 class OrdersController extends Controller
 {
@@ -17,7 +19,6 @@ class OrdersController extends Controller
      */
     public function index()
     {
-        //
     }
 
     /**
@@ -49,8 +50,8 @@ class OrdersController extends Controller
             'total' => $sub_total, // missing
             'store_id' => $request->store_id,
             'voucher_id' => $request->voucher_id,
-            'customer_id' => 3, // missing
-            'payment_type' => $payment_type, // missing
+            'customer_id' => $request->customer_id, // missing
+            'payment_type' => $payment_type,        // missing
             'transaction_code' => $transaction_code,
         ]);
 
@@ -65,6 +66,13 @@ class OrdersController extends Controller
         }
 
         $momoPayment = new MOMOController;
+
+        History::create([
+            'order_id' => $order_id,
+            'status_id' => 0,
+            'transaction_id' => 0,
+            'delivery_id' => 0,
+        ]);
 
         return response()->json([
             'message' => 'Create new order successfully!',
@@ -106,5 +114,43 @@ class OrdersController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function viewHistory(Request $request)
+    {
+        $customer_id = $request->customer_id;
+        $order_ids = Order::where('customer_id', $customer_id)->pluck('id');
+
+        $order_progresses = [
+            '000' => 'Order was placed successfully.',
+            '030' => 'Order was paid successfully.',
+            '020' => 'Order was paid unsuccessfully.',
+        ];
+        $histories = [];
+        foreach ($order_ids as $order_id) {
+            $customer_history = History::select('order_id', 'status_id', 'transaction_id', 'delivery_id', 'created_at')
+                ->where('order_id', $order_id)->get();
+
+            $history = new stdClass;
+            $order_info = new stdClass;
+            $progresses = [];
+            foreach ($customer_history as $history_item) {
+                $order_progress = $history_item->status_id . $history_item->transaction_id . $history_item->delivery_id;
+
+                $progress_object = (object) [
+                    'order_progress' => $order_progresses[$order_progress],
+                    'timestamp' => $history_item->created_at->toDateTimeString(),
+                ];
+                $progresses[] = $progress_object;
+
+                $order_info->order_id = $history_item->order_id;
+            }
+            $history->order_info = $order_info;
+            $history->progresses = $progresses;
+
+            $histories[] = $history;
+        }
+
+        return $histories;
     }
 }
